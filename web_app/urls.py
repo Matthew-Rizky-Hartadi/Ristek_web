399b8ba4ceb8cb6f5fe0bbc88556648429334b45
unknown <matthewrizkyhartadi@gmail.com>
2023-02-22 08:16:26 +0700

    added authentication, signup/login page, and post ajax

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from web_app.views import show_home, show_profile
+from web_app.views import show_home, show_profile, non_user_home, register, login_user,show_data_json, add_post, add_post2
 
 app_name = 'web_app'
 
 urlpatterns = [
-    path('', show_home, name='show_home'),
-    path('profile', show_profile, name='show_profile')
+    path('home', show_home, name='show_home'),
+    path('profile', show_profile, name='show_profile'),
+    path('', non_user_home, name="non_user_home"),
+    path('register', register, name='register'),
+    path('login', login_user, name='login'),
+    path('json',show_data_json,name="json"),
+    path('add_post', add_post, name="add_post"),
+    path('add_post2', add_post2, name="add_post2"),
+
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,8 +1,104 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+import datetime
+from django.db import transaction
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.core import serializers
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def show_home(request):
-    return render(request, "home.html")
+    Posts = Post.objects.all()
+    Posts.reverse()
+    print(Posts)
+    context= {
+        'user': request.user,
+        'posts': Posts,
+    }
+    return render(request, "home.html", context)
 
 def show_profile(request):
+    Posts = Post.objects.filter(user = request.user)
+    
     return render(request, "profile.html")
+
+def non_user_home(request):
+    Posts = Post.objects.all()
+    print(Posts)
+    context= {
+        'posts' : Posts,
+    }
+    return render(request, "non_user_home.html", context)
+
+def register(request):
+    form= UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Axcount successfully created!')
+            return redirect('web_app:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("web_app:show_home")) # create response
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return redirect('web_app:show_home')
+            
+        else:
+            messages.info(request, 'Wrong Username or Password!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def add_post(request):
+
+    if request.method == "POST":
+        post = request.POST.get('content')
+        if post != "" and request.user != None:
+            print(f"Adding post with content '{post}' and user {request.user.username}")
+            data = Post.objects.create(user=request.user, date=datetime.datetime.today(),content=post)
+            print(f"Post added with id {data.id}")
+            return JsonResponse({
+                    "pk": data.id,
+                    "fields": {
+                        "user": data.user.username,
+                        "content": data.content,
+                        "date": data.date.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                    },
+                },
+                status=200,
+            )
+        else:
+            messages.info(request, "Please add a post")
+
+def add_post2(request):
+
+    if request.method == "POST":
+        post = request.POST.get('post')
+        if post != "" and request.user != None:
+            Post.objects.create(user=request.user, date=datetime.datetime.today(),content=post)
+        elif request.user == None:
+            Post.objects.create(date=datetime.datetime.today(),content=post)
+        else:
+            messages.info(request, "Please add a post")
+    
+    return render(request, 'non_user_home.html')
+
+def show_data_json(request):
+    data = Post.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
