ccadf8709490099f1b7b5909c7e0bf28bc6226bc
unknown <matthewrizkyhartadi@gmail.com>
2023-02-28 08:51:23 +0700

    Implemented post and delete

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from web_app.views import show_home, show_profile, non_user_home, register, login_user,show_data_json, add_post, add_post2
+from web_app.views import show_home, show_profile, non_user_home, register, login_user,show_data_json, add_post, add_post2, logout_user, delete_post, delete_A, delete_User
 
 app_name = 'web_app'
 
@@ -12,5 +12,9 @@ urlpatterns = [
     path('json',show_data_json,name="json"),
     path('add_post', add_post, name="add_post"),
     path('add_post2', add_post2, name="add_post2"),
+    path('logout', logout_user, name='logout'),
+    path('delete/<int:id>', delete_post, name='delete'),
+    path('deleteA/<int:id>', delete_A, name='delete_A'),
+    path('deleteU/<int:id>', delete_User, name='delete_U'),
 
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -2,37 +2,63 @@ from django.shortcuts import render, redirect
 from .models import Post
 import datetime
 from django.db import transaction
+import json
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.core import serializers
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
+from django.forms.models import model_to_dict
+from django.contrib.auth.models import AnonymousUser, User
 
 # Create your views here.
 def show_home(request):
     Posts = Post.objects.all()
-    Posts.reverse()
-    print(Posts)
+    User = get_user_model()
+    users = User.objects.all()
+    print(users)
+    Posts_reversed = []
+    for i in range(len(Posts)):
+        index = len(Posts) - i - 1
+        Posts_reversed.append(Posts[index])
+    # print(Posts_reversed)
     context= {
         'user': request.user,
-        'posts': Posts,
+        'posts': Posts_reversed,
     }
     return render(request, "home.html", context)
 
 def show_profile(request):
     Posts = Post.objects.filter(user = request.user)
+    Posts_reversed = []
+    for i in range(len(Posts)):
+        index = len(Posts) - i - 1
+        Posts_reversed.append(Posts[index])
+    context= {
+        'user': request.user,
+        'posts': Posts_reversed,
+    }
+    print(Posts_reversed)
     
-    return render(request, "profile.html")
+    return render(request, "profile.html", context)
 
 def non_user_home(request):
     Posts = Post.objects.all()
-    print(Posts)
+    User = get_user_model()
+    users = User.objects.all()
+    user = User.objects.get(username='anonymous')
+    print(users[2])
+    Posts_reversed = []
+    for i in range(len(Posts)):
+        index = len(Posts) - i - 1
+        Posts_reversed.append(Posts[index])
     context= {
-        'posts' : Posts,
+        'posts' :Posts_reversed,
+        'user': user,
     }
     return render(request, "non_user_home.html", context)
 
@@ -66,17 +92,23 @@ def login_user(request):
     return render(request, 'login.html', context)
 
 def add_post(request):
-
+    print(request.method)
+    print(request.POST)
     if request.method == "POST":
         post = request.POST.get('content')
         if post != "" and request.user != None:
-            print(f"Adding post with content '{post}' and user {request.user.username}")
             data = Post.objects.create(user=request.user, date=datetime.datetime.today(),content=post)
-            print(f"Post added with id {data.id}")
+            print(type(data))
+            
+            # user = (User.objects.filter(pk=request.user.pk).values("username").first())
+            # print(user)
+            # user_dict = model_to_dict(data.user)
+            # print(user_dict)
+            # print(user_dict['username'])
             return JsonResponse({
                     "pk": data.id,
                     "fields": {
-                        "user": data.user.username,
+                        "user": 'matt',
                         "content": data.content,
                         "date": data.date.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                     },
@@ -85,20 +117,64 @@ def add_post(request):
             )
         else:
             messages.info(request, "Please add a post")
+    # return render(request, 'home.html')
+
 
 def add_post2(request):
-
+    print(request.user)
     if request.method == "POST":
-        post = request.POST.get('post')
-        if post != "" and request.user != None:
-            Post.objects.create(user=request.user, date=datetime.datetime.today(),content=post)
-        elif request.user == None:
-            Post.objects.create(date=datetime.datetime.today(),content=post)
+        post = request.POST.get('content')
+        print(post)
+        if post != "":
+            try:
+                user = User.objects.get(username='anonymous')
+            except User.DoesNotExist:
+                user = User.objects.create_user(username='anonymous', email='anonymous@example.com', password='password')
+            data = Post.objects.create(user=user,date=datetime.datetime.today(), content=post)
+            return JsonResponse({
+                    "pk": data.id,
+                    "fields": {
+                        "user": data.user,
+                        "content": data.content,
+                        "date": data.date.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                    },
+                },
+                status=200,
+            )
         else:
             messages.info(request, "Please add a post")
     
-    return render(request, 'non_user_home.html')
+    # return render(request, 'non_user_home.html')
 
 def show_data_json(request):
     data = Post.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def delete_post(request, id):
+    post = Post.objects.get(user = request.user, id=id)
+    user_dict = model_to_dict(post.user)
+    post.delete()
+    return redirect('web_app:show_profile')
+
+def logout_user(request):
+    logout(request)
+    return redirect('web_app:non_user_home')
+
+def delete_A(request, id):
+    post = Post.objects.get(id=id)
+    print(post)
+    post.delete()
+
+    return redirect('web_app:non_user_home')
+
+def delete_User(request, id):
+    post = Post.objects.get(user = request.user, id=id)
+    user_dict = model_to_dict(post.user)
+    post.delete()
+    return redirect('web_app:show_home')
+
+def edit_post(request, id):
+    post = Post.objects.get(user = request.user, id=id)
+    
+
+    
